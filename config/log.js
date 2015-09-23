63a72c985d61b242861c3721200a25c0eb51887b
keke <zhoukekestar@163.com>
2015-09-23 20:17:08 +0800

    add SLS

---
+++ config/log.js
@@ -0,0 +1,41 @@
+
+if (process.env.NODE_ACE !== undefined) {
+
+  console.log('log.js exports ACE env. ACE!!')
+  module.exports = {
+
+    signCode: 'your-sign-code',
+    project: 'your-project-name',
+    hostname: 'localhost',
+    port: 3000,
+    path: '/MongoDB/logs',
+    debug: true
+  }
+
+} else if (process.env.OS !== 'Windows_NT') {
+
+  console.log('log.js exports TEST env. TEST!!');
+
+  module.exports = {
+
+    signCode: 'your-sign-code',
+    project: 'your-project-name',
+    hostname: 'localhost',
+    port: 3000,
+    path: '/MongoDB/logs',
+    debug: true
+  }
+
+} else {
+  console.log('log.js exports local env. LOCAL!!');
+
+  module.exports = {
+
+    signCode: 'your-sign-code',
+    project: 'your-project-name',
+    hostname: 'localhost',
+    port: 3000,
+    path: '/MongoDB/logs',
+    debug: true
+  }
+}
