2e50f7b1a4ee83ec9b9482e2b43e167e8c554448
keke <zhoukekestar@163.com>
2015-10-28 11:23:34 +0800

    Ajax support.

--- routes/wechat/sdk.js
+++ routes/wechat/sdk.js
@@ -3,9 +3,26 @@ var router    = express.Router();
 var config    = require('../../config/wechat')
 var wechat    = require('../../lib/wechat');
 
+
+// Can be access by ajax
+router.options('/sign', function(req, res) {
+  res.set({
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'origin, authorization, accept, content-type, x-requested-with',
+    'Access-Control-Allow-Methods': 'GET, HEAD, POST, PUT, DELETE, TRACE, OPTIONS'
+  });
+  res.end();
+})
+
 router.get('/sign', function(req, res){
 
   wechat.getSign(req.query.url, function(time, random, sign) {
+
+    res.set({
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'origin, authorization, accept, content-type, x-requested-with',
+      'Access-Control-Allow-Methods': 'GET, HEAD, POST, PUT, DELETE, TRACE, OPTIONS'
+    });
 
     var d = {
       url: req.query.url,
